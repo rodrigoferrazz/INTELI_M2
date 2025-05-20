7d377fb537a6f61dfbc90a52a5a72b3ebb8aa306
Rodrigo Ferraz <rodrigoferraz@Rodrigos-Macbook-Air.local>
2025-05-20 11:25:45 -0300

    adiciona aula 06

--- exercicio-aula-01-cristiano/controllers/cursoController.js
+++ exercicio-aula-01-cristiano/controllers/cursoController.js
@@ -5,7 +5,7 @@ exports.create = async (req, res) => {
   await Curso.create(nome);
   res.redirect('/alunos');
 };
-// Controller que recebe os dados do formulário e chama o model para atualizar o curso
+
 exports.update = async (req, res) => {
   const { id } = req.params;
   const { nome } = req.body;
@@ -13,7 +13,6 @@ exports.update = async (req, res) => {
   res.redirect('/alunos');
 };
 
-// Controller que chama o model para deletar o curso e redireciona
 exports.delete = async (req, res) => {
   const { id } = req.params;
   await Curso.delete(id);
