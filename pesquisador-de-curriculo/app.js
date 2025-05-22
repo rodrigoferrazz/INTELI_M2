1a743d07a500cac0e595f7704af26d3742611d7b
Rodrigo Ferraz <rodrigoferraz@Rodrigos-Macbook-Air.local>
2025-05-22 09:36:08 -0300

    .

--- pesquisador-de-curriculo/app.js
+++ pesquisador-de-curriculo/app.js
@@ -1,11 +1,10 @@
-// app.js
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');       // ← deve apontar para routes/index.js
+const routes = require('./routes/index.js');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', routes);                    // ← monta as rotas na raiz
+app.use('/', routes);
 app.listen(3000, () => console.log('http://localhost:3000'));
--- pesquisador-de-curriculo/scripts/init.sql
+++ pesquisador-de-curriculo/scripts/init.sql
@@ -1,8 +1,25 @@
 CREATE TABLE IF NOT EXISTS curriculos (
-    id INT PRIMARY KEY, 
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL, 
     area_de_interesse VARCHAR(50) NOT NULL,  
     idade INT NOT NULL,  
     numero VARCHAR(20) NOT NULL,
     arquivo VARCHAR(255) NOT NULL
 );
+
+INSERT INTO curriculos (nome, area_de_interesse, idade, numero, arquivo) VALUES
+('Marcela Cruz', 'Financeiro', 45, '(+55) 14 141414141', 'marcelacruz.pdf'),
+('Samuel Oliveira', 'Administrativo', 23, '(+55) 14 141414142', 'samueloliveira.pdf'),
+('Ronaldo Campos', 'Recursos Humanos', 33, '(+55) 14 141414143', 'ronaldocampos.pdf'),
+('Erick Santos', 'Recursos Humanos', 22, '(+55) 14 141414144', 'ericksantos.pdf'),
+('Sara Almeida', 'Financeiro', 44, '(+55) 14 141414145', 'saraalmeida.pdf'),
+('Victoria Gava', 'Financeiro', 35, '(+55) 14 141414146', 'victoriagava.pdf'),
+('Rodrigo Silva', 'Administrativo', 34, '(+55) 14 141414147', 'rodrigosilva.pdf'),
+('Adriana Camargo', 'Administrativo', 34, '(+55) 14 141414148', 'adrianacamargo.pdf'),
+('Joao Lonn', 'Recepcionista', 34, '(+55) 14 141414149', 'joaolonn.pdf'),
+('Bernardo Meirelles', 'Motorista', 23, '(+55) 14 141414150', 'bernardomeirelles.pdf'),
+('Danilo de Castro', 'Gestor de Tráfego', 21, '(+55) 14 141414151', 'danilodecastro.pdf'),
+('Victor Raymundo', 'Analista de Dados', 22, '(+55) 14 141414152', 'victorraymundo.pdf'),
+('Guilherme Holanda', 'Design Gráfico', 41, '(+55) 14 141414153', 'guilhermeholanda.pdf'),
+('Leandro Pozza', 'Marketing', 37, '(+55) 14 141414154', 'leandropozza.pdf'),
+('Arthur Ossucci', 'Marketing', 36, '(+55) 14 141414155', 'arthurossucci.pdf');
